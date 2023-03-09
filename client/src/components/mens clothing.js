fc5af9c6f14d472ff5d3e93cb7bd7d66434cc4a3
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-09 13:41:58 -0800

    mensclothingjs and Womensclothingjs formatted

---
+++ client/src/components/mens clothing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function mensClothingInventory() {
+    const mensShirt = ['Polo', 'Button Down', 'Jersey']
+    const mensClothingInventory = mensShirt.map(mensShirt => <h2>{mensShirt}</h2>)
+return <div>{mensClothingInventory}</div>}
+
+function mensClothingInventory() {
+    const mensBottoms = ['Jeans', 'Slacks', 'Jersey']
+    const mensClothingInventory = mensPants.map(mensPants => <h2>{mensPants}</h2>)
+return <div>{mensClothingInventory}</div>}
+
+function mensClothingInventory() {
+    const mensShoes = ['sneakers', 'formal', 'Slides']
+    const mensClothingInventory = mensShoes.map(mensShoes => <h2>{mensShoe}</h2>)
+return <div>{mensClothingInventory}</div>}
+
+export default mensClothingInventory
---
+++ client/src/components/womens clothing.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+function womensClothingInventory() {
+    const womensShirt = ['Blouse', 'Crop Top', 'Tank Top']
+    const womensClothingInventory = womensShirt.map(womensShirt => <h2>{womensShirt}</h2>)
+return <div>{womensClothingInventory}</div>}
+
+function womensClothingInventory() {
+    const womensBottoms = ['Jeans', 'Dress Pants', 'Mini Skirt']
+    const womensClothingInventory = womensBottoms.map(womensBottoms => <h2>{womensBottoms}</h2>)
+return <div>{womensClothingInventory}</div>}
+
+function womensClothingInventory() {
+    const womensShirt = ['Blouse', 'Crop Top', 'Tank Top']
+    const womensClothingInventory = womensShirt.map(womensShirt => <h2>{womensShirt}</h2>)
+return <div>{womensClothingInventory}</div>}
+
+export default womensClothingInventory
