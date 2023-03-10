442b03b73d73302303ccf3e5fbd98f743d10f427
Mariama <mariamabah@Mariamas-MacBook-Pro.local>
2023-03-09 21:22:47 -0500

    shop data

---
+++ client/src/components/shopdata.js
@@ -0,0 +1,132 @@
+const SHOP_DATA = [
+
+    {
+            Title: "Men's shirts",
+            items: 
+        {
+            id: 0,
+            name: "Polo",
+            price: 35,
+        },
+    },
+        {
+            id: 1,
+            name:"Button down",
+            price: 20,
+        },
+        {
+            id: 2,
+            name: "Jersey",
+            price: 50,
+        }, 
+        {
+            Title: "Men's bottom",
+            items: 
+        {
+            id: 3,
+            name: "Jeans",
+            price: 30,
+        },
+            
+        },
+        {
+            id: 4,
+            name: "Slacks",
+            price: 60,
+        },
+        {
+            id: 5,
+            name: "Jersey",
+            price: 25,
+        },
+        {
+            Title: "Men's shoes",
+            items:
+          {
+            id: 6,
+            name: "Sneakers",
+            price: 100,
+          },
+        },
+            
+        {
+            id: 7,
+            name: "Formal",
+            price: 60,
+
+        },
+        {
+            id: 8,
+            name: "Slides",
+            price: 10,
+        },
+        {
+            Title: "Women's shirt",
+            items:
+           {
+            id: 9,
+            name: "Blouse",
+            price: 15,
+           },
+        },
+            {id: 10,
+            name: "Croptop",
+            price: 5,
+        },
+        {
+            id: 11,
+            name: "Tanktop"
+            price: 20,
+        },
+        {
+            Title: "Women's bottom",
+            items: 
+              
+                {id: 12,
+                name: "Jeans",
+                price: 35,
+              },
+              
+                id: 13, 
+                name: "Dress pants",
+                price: 40,
+              },
+              {
+                id: 14, 
+                name: "Mini skirt",
+                price: 15,
+            },
+        {
+            Title: "Women's shoes",
+                items:
+                
+                    {id: 15,
+                    name: "Heels",
+                    price: 55,
+            },
+                
+                   id: 16,
+                    name: "Flats",
+                    price: 20,
+                },
+                
+                    {id: 17,
+                    name: "Sneakers",
+                    price: 80,
+                },
+            
+        
+        
+        
+    
+    
+
+];
+
+    
+  
+
+
+
+
+    export default SHOP_DATA;
